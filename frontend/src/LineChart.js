05e930cbdc29cab69eb847e466afb2ec4a1a1ece
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-11 20:40:43 -0300

    Finalizado a base do gráfico de cotações

--- frontend/src/LineChart.js
+++ frontend/src/LineChart.js
@@ -1,4 +1,22 @@
 import React, { Component } from 'react';
+import CanvasJSReact from './assets/canvas.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+var openValues = [{
+    x: 0.222,
+    y: new Date()
+},{
+    x: 0.221,
+    y: new Date()
+}];
+
+var closeValues = [{
+    x: 0.122,
+    y: new Date()
+},{
+    x: 0.121,
+    y: new Date()
+}];
 
 class LineChart extends Component{
     constructor(){
@@ -11,12 +29,44 @@ class LineChart extends Component{
     }
 
     updateChart(){
-        console.log('teste');
+        this.chart.render();
     }
 
     render() {
+        const options = {
+            title:{
+                text: 'Stock Quotes'
+            },
+            axisY: {
+                includeZero: false,
+                valueFormatString: '0.##0',
+                title: 'Price in (USD)'
+            },
+            axisX: {
+                valueFormatString: 'HH:mm',
+            },
+            data:[{
+                type: 'line',
+                name: 'Open Value',
+                showInLegend: true,
+                yValueFormatString: '0.##0',
+                xValueFormatString: 'HH:mm',
+                dataPoints: openValues
+            },{
+                type: 'line',
+                name: 'Close Value',
+                showInLegend: true,
+                yValueFormatString: '0.##0',
+                xValueFormatString: 'HH:mm',
+                dataPoints: closeValues
+            }]    
+        };
+
         return (
-        <div>Gráfico</div>
+        <div>
+            <h1>Acompanhamento de cotação</h1>
+            <CanvasJSChart options={options} onRef={ref => this.chart = ref}/> 
+        </div>
         );
 	}
 
