eec64d33a87eff9fdab5794131b0836f534a6e98
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-14 10:35:37 -0300

    Iniciando um novo backend usando Spring WebFlux

--- backend/src/main/java/com/digitalinnovation/one/stockquotesapi/StockQuotesApiApplication.java
+++ backend/src/main/java/com/digitalinnovation/one/stockquotesapi/StockQuotesApiApplication.java
@@ -1,52 +1,13 @@
 package com.digitalinnovation.one.stockquotesapi;
 
-import lombok.extern.log4j.Log4j2;
-import org.apache.commons.math3.random.RandomDataGenerator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.Date;
-
-@Log4j2
-@EnableScheduling
 @SpringBootApplication
 public class StockQuotesApiApplication {
 
-	@Autowired
-	QuoteRepository quoteRepository;
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(StockQuotesApiApplication.class, args);
 	}
 
-	@Scheduled(fixedDelay = 1000)
-	public void generateData() {
-		log.info(quoteRepository.findFirstBySymbolOrderByTimestampDesc("TESTE")
-						.map(this::generateNewData)
-						.orElseGet(this::initializeData));
-	}
-
-	private Quote initializeData() {
-		Quote quoteToSave = Quote.builder()
-				.symbol("TESTE")
-				.openValue(0.2222222)
-				.closeValue(0.2222222)
-				.timestamp(new Date())
-				.build();
-		return quoteRepository.save(quoteToSave);
-	}
-
-	private Quote generateNewData(Quote quote) {
-		Quote quoteToSave = Quote.builder()
-				.symbol(quote.getSymbol())
-				.openValue(quote.getOpenValue() * new RandomDataGenerator().nextUniform(-0.1, 0.1))
-				.closeValue(quote.getCloseValue() * new RandomDataGenerator().nextUniform(-0.1, 0.1))
-				.timestamp(quote.getTimestamp())
-				.build();
-		return quoteRepository.save(quoteToSave);
-	}
 }
