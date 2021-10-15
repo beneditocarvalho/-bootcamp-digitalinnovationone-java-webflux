ed1f2fe0ded7a2ee732668f6ee36545ad0fa22df
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-14 21:37:26 -0300

    Atualizado QuoteGenerator para gerar quotes ininterruptamente

--- backend/src/main/java/com/digitalinnovation/one/stockquotesapi/QuoteGenerator.java
+++ backend/src/main/java/com/digitalinnovation/one/stockquotesapi/QuoteGenerator.java
@@ -1,38 +1,64 @@
 package com.digitalinnovation.one.stockquotesapi;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.math3.random.RandomDataGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 
+import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Log4j2
-@Component
-public class QuoteGenerator implements ApplicationRunner {
-    @Autowired
-    private QuoteRepository repository;
+@Service
+@Transactional
+@AllArgsConstructor
+public class QuoteGenerator {
+    private final QuoteRepository repository;
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        log.info("Starting data insertion");
+    @PostConstruct
+    public void init() {
+        Flux.generate(() -> {
+                    log.info("Starting data insertion");
+                    return initialQuote();
+                }, (state, sink) -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    sink.next(state);
+                    return createNewQuote(state);
+                })
+                .delaySubscription(Duration.ofMillis(3000))
+                .subscribe();
+    }
 
-        var quotes = Quote.builder()
-                .symbol("TESTE")
-                .openValue(0.2)
-                .closeValue(0.2)
+    private Quote createNewQuote(Quote previousQuote) {
+        var newQuote = Quote.builder()
+                .symbol(previousQuote.getSymbol())
+                .openValue(previousQuote.getOpenValue() + new RandomDataGenerator().nextUniform(-0.1, 0.1))
+                .closeValue(previousQuote.getCloseValue() + new RandomDataGenerator().nextUniform(-0.1, 0.1))
                 .timestamp(LocalDateTime.now())
                 .build();
+        repository.save(newQuote).subscribe(log::info);
+        return newQuote;
+    }
 
-        repository.save(quotes)
-                .subscribe(
-                        f -> repository.findAll()
-                                .subscribe(
-                                        q -> log.info("Quote {}", q),
-                                        e -> log.error("findAll Error: {}", e.getMessage())
-                                ),
-                        e -> log.error("save Error: {}", e.getMessage())
-                );
+    private Quote initialQuote() {
+        var quote = Quote.builder()
+                .openValue(0.2)
+                .closeValue(0.2)
+                .symbol("TESTE")
+                .timestamp(LocalDateTime.now())
+                .build();
+        repository.save(quote).subscribe(log::info);
+        return quote;
     }
 }
